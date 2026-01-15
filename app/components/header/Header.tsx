4a35d4f4da5cc7c84d79211eae7fe7a134e3be79
h-khalid-h <h.khalid@datac.com>
2026-01-15 09:20:03 +0200

    Simplified: Removed ChatDescription and HeaderActionButtons from Header, testing full app

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,8 +1,6 @@
 import { useStore } from '@nanostores/react';
 import { chatStore } from '~/lib/stores/chat';
 import { classNames } from '~/utils/classNames';
-import { HeaderActionButtons } from './HeaderActionButtons.client';
-import { ChatDescription } from '~/lib/persistence/ChatDescription.client';
 import { Tooltip } from '~/components/ui/Tooltip';
 
 export function Header() {
@@ -27,13 +25,8 @@ export function Header() {
         </div>
       </Tooltip>
       <span className="flex-1 px-4 truncate text-center text-bolt-elements-textPrimary">
-        <ChatDescription />
+        Shining - Intelligent Outcome Platform
       </span>
-      {chat.started && (
-        <div className="mr-1">
-          <HeaderActionButtons />
-        </div>
-      )}
     </header>
   );
 }
